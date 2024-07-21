21a95b89e6415ccbabe5fdec06ac36a3349ab3b6
Geunwon Mo <gn908925@dal.ca>
2024-07-21 22:41:38 +0900

    test for adding driver

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-'use strict';
+"use strict";
 
-const Homey = require('homey');
-
+const Homey = require("homey");
+// const { debug } = require("zigbee-clusters");
+// debug(true);
 class MyApp extends Homey.App {
-
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log("MyApp has been initialized");
   }
-
 }
 
 module.exports = MyApp;
